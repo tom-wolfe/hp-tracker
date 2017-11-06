2be4b9de0e53f9763d3bc3abf4eb530a6f3136bc
Tom Wolfe <twolfe@scottlogic.co.uk>
2017-11-06 13:30:58 +0000

    Add localstorage metareducer.

--- src/app/tracker/tracker.module.ts
+++ src/app/tracker/tracker.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 import { SharedModule } from '../shared/shared.module';
 import { hpReducer } from './state/hp.reducer';
 import { TrackerRoutingModule } from './tracker-routing.module';
 import { TrackerComponent } from './tracker.component';
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: ['hp'], rehydrate: true })(reducer);
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 const COMPONENTS = [
   TrackerComponent
@@ -17,7 +23,7 @@ const COMPONENTS = [
   imports: [
     SharedModule,
     TrackerRoutingModule,
-    StoreModule.forFeature('tracker', { hp: hpReducer }),
+    StoreModule.forFeature('tracker', { hp: hpReducer }, { metaReducers }),
   ],
   providers: []
 })
