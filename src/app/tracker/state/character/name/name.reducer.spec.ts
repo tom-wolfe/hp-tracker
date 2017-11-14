926ffdde21683ea537c442b5c72d80ef2dc8f84b
Tom Wolfe <twolfe@scottlogic.co.uk>
2017-11-14 09:38:37 +0000

    Fix name tests.

--- src/app/tracker/state/character/name/name.reducer.spec.ts
+++ src/app/tracker/state/character/name/name.reducer.spec.ts
@@ -1,102 +1,20 @@
-import * as HP from './';
+import * as Name from './';
 
 describe('hpReducer', () => {
   describe('undefined action', () => {
     it('should return the default state', () => {
       const action = {} as any;
-      const result = HP.hpReducer(undefined, action);
-      expect(result).toEqual(HP.initialState);
+      const result = Name.nameReducer(undefined, action);
+      expect(result).toEqual('Paladin');
     });
   });
 
-  describe('heal action', () => {
-    it('should not heal over max', () => {
-      const action = new HP.Heal(10);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 45,
-        maxHp: 50,
-        tempHp: 0,
-        tempMaxHp: 0
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(50);
-    });
-    it('should not heal into temp', () => {
-      const action = new HP.Heal(10);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 45,
-        maxHp: 50,
-        tempHp: 5,
-        tempMaxHp: 0
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(50);
-    });
-    it('should heal into temp max', () => {
-      const action = new HP.Heal(10);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 45,
-        maxHp: 50,
-        tempHp: 0,
-        tempMaxHp: 5
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(55);
-    });
-    it('should not heal over temp max', () => {
-      const action = new HP.Heal(20);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 45,
-        maxHp: 50,
-        tempHp: 0,
-        tempMaxHp: 5
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(55);
-    });
-  });
-  describe('hurt action', () => {
-    it('should not hurt below min', () => {
-      const action = new HP.Hurt(50);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 25,
-        maxHp: 50,
-        tempHp: 0,
-        tempMaxHp: 0
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(0);
-    });
-    it('should hurt temp hp first', () => {
-      const action = new HP.Hurt(10);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 25,
-        maxHp: 50,
-        tempHp: 10,
-        tempMaxHp: 0
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(25);
-      expect(result.tempHp).toEqual(0);
-    });
-    it('should carry hurt over after temp hp', () => {
-      const action = new HP.Hurt(20);
-      const state: HP.HPState = {
-        character: 'Paladin',
-        currentHp: 25,
-        maxHp: 50,
-        tempHp: 10,
-        tempMaxHp: 5
-      };
-      const result = HP.hpReducer(state, action);
-      expect(result.currentHp).toEqual(15);
-      expect(result.tempHp).toEqual(0);
+  describe('set action', () => {
+    it('should set name', () => {
+      const action = new Name.SetName('Jeff');
+      const state = 'George';
+      const result = Name.nameReducer(state, action);
+      expect(result).toEqual('Jeff');
     });
   });
 });
